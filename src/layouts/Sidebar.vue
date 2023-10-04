8d0e726afe4816de87b05068c3da5569c497ca5f
Dario Contreras <thedaroo81@gmail.com>
2023-10-04 00:05:44 -0300

    Actualización Sidebar
    Se actualizó la sidebar para que sea más parecida al mokup

--- src/layouts/Sidebar.vue
+++ src/layouts/Sidebar.vue
@@ -5,9 +5,10 @@
 
 <template>
     <div class="flex flex-col h-screen">
-        <div id="UserOptions" class="h-[50%] pl-6 w-[240px] bg-black">
-            <RouterLink to="/">
-                <img width="125" src="/images/icons/audioflow-logo.png" class="pl-10 ml-2 mt-14">
+        <div id="UserOptions" class="pl-6 w-[240px] bg-black">
+            <RouterLink to="/"  class="flex items-center pt-6">
+                <img width="55" src="/images/icons/audioflow-logo.png" class="">
+                <span class="font-sans font-bold text-white text-[25px]">AudioFlow</span>
             </RouterLink>
             <div class="my-8 pt-1"></div>
             <ul>
@@ -20,21 +21,22 @@
                 <RouterLink to="/library">
                     <MenuItem class="ml-[1px]" :iconSize="26" name="Tus canciones" iconString="library" pageUrl="/library" />
                 </RouterLink>
-                <div class="border-b border-b-gray-700 mt-1"></div>
-                <div class="my-4">
-                    <MenuItem :iconSize="24" name="Crear Playlist" iconString="playlist" pageUrl="/playlist" />
-                    <MenuItem :iconSize="24" name="Subir Canción" iconString="playlist" pageUrl="/liked" class="pb-0"/>
+                <div>
+                    <MenuItem class="ml-[1px]" :iconSize="24" name="Crear Playlist" iconString="playlist" pageUrl="/playlist" />
                 </div>
-                <div class="border-b border-b-gray-700"></div>
+                <div>
+                    <MenuItem :iconSize="24" name="Subir Canción" iconString="upload" pageUrl="/liked" class="ml-[1px]"/>
+                </div>
             </ul>
         </div>
-        <div id="UserPlaylists" class="h-[50%] pl-6 w-[240px] bg-black">
-            <span class="text-white">Mis playlists</span>
+        <div id="UserPlaylists" class="h-[100%] pl-6 w-[240px] bg-black">
+            
+            <div class="border-b border-b-gray-700 w-[200px]"></div>
                 <ul>
-                    <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">Mi Playlist #1</li>
-                    <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">Mi Playlist #2</li>
-                    <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">Mi Playlist #3</li>
-                    <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">Mi Playlist #4</li>
+                    <li class="font-semibold text-[15px] mt-3 text-gray-300 hover:text-white">Tu Playlist #1</li>
+                    <li class="font-semibold text-[15px] mt-3 text-gray-300 hover:text-white">Tu Playlist #2</li>
+                    <li class="font-semibold text-[15px] mt-3 text-gray-300 hover:text-white">Tu Playlist #3</li>
+                    <li class="font-semibold text-[15px] mt-3 text-gray-300 hover:text-white">Tu Playlist #4</li>
                 </ul>
         </div>
     </div>
