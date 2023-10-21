86dcdcb97b70589081200edc650b898bba37227d
mizok <cairamgo@gmail.com>
2023-10-21 04:04:10 -0300

    cambio en songcard.

--- src/components/SongCard.vue
+++ src/components/SongCard.vue
@@ -3,7 +3,9 @@
         <div class="bg-[#181818] hover:bg-[#282828] p-4 rounded-md m-2 shadow-xlhover:bg-[#252525] cursor-pointer h-[250px] w-[130px] sm:w-[180px] group transition-all duration-300 ease-in-out">
             <div class="flex justify-center items-center relative h-[150px] w-[100px] sm:w-[148px] transition-all duration-300 ease-in-out">
                 <img class="rounded-md shadow-2xl" :src="pic_url" alt="">
-                <img class="absolute bottom-0 right-0 h-[45px] w-[45px] rounded-md shadow-2xl hover:scale-110 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out mb-3 mr-2" src="/images/icons/play-icon.png" alt="">
+                <img class="absolute bottom-0 right-0 mb-8 mr-1 h-[35px] w-[35px] rounded-md shadow-2xl
+                sm:mb-3 sm:mr-2 sm:h-[45px] sm:w-[45px]
+                hover:scale-110 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out" src="/images/icons/play-icon.png" alt="">
             </div>
             <div class="text-white font-semibold text-[17px] sm:pt-4 pl-1">{{ name }}</div>
             <div class="text-gray-400 font-semibold text-[14px] sm:pt-1 pb-3 pl-1">
