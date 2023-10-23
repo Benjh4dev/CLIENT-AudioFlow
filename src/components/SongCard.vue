5a186219c45e7469af6fd2df3027f5fcaf69fe50
mizok <cairamgo@gmail.com>
2023-10-23 20:42:04 -0300

    Se modifica SongCard para mostrar las canciones del sistema.

--- src/components/SongCard.vue
+++ src/components/SongCard.vue
@@ -1,14 +1,14 @@
 <template>
     <RouterLink to="library">
-        <div class="bg-[#181818] hover:bg-[#282828] p-4 rounded-md m-2 shadow-xlhover:bg-[#252525] cursor-pointer h-[250px] w-[130px] sm:w-[180px] group transition-all duration-300 ease-in-out">
+        <div class="bg-[#181818] hover:bg-[#282828] p-4 rounded-md m-2 shadow-xlhover:bg-[#252525] cursor-pointer h-[260px] w-[130px] sm:w-[180px] group transition-all duration-300 ease-in-out">
             <div class="flex justify-center items-center relative h-[150px] w-[100px] sm:w-[148px] transition-all duration-300 ease-in-out">
-                <img class="rounded-md shadow-2xl" :src="pic_url" alt="">
+                <img class="rounded-md shadow-2xl" :src="coverURL" alt="">
                 <img class="absolute bottom-0 right-0 mb-8 mr-1 h-[35px] w-[35px] rounded-md shadow-2xl
                 sm:mb-3 sm:mr-2 sm:h-[45px] sm:w-[45px]
                 hover:scale-110 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out" src="/images/icons/play-icon.png" alt="">
             </div>
-            <div class="text-white font-semibold text-[17px] sm:pt-4 pl-1">{{ name }}</div>
-            <div class="text-gray-400 font-semibold text-[14px] sm:pt-1 pb-3 pl-1">
+            <div class="text-white font-semibold text-[15px] sm:pt-4 pl-1">{{ name }}</div>
+            <div class="text-gray-400 font-semibold text-[12px] sm:pt-1 pb-3 pl-1">
                 <h6 class="hover:underline">
                     {{ artist }}
                 </h6>
@@ -20,11 +20,15 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { RouterLink } from 'vue-router';
+import { useSongStore } from '@/stores/song';
+
+const songStore = useSongStore()
 
 const props = defineProps({
-    pic_url: String,
+    audioURL: String,
+    coverURL: String,
     name: String,
     artist: String
 })
-const { pic_url, name, artist } = toRefs(props)
+const { coverURL, name, artist, audioURL } = toRefs(props)
 </script>
