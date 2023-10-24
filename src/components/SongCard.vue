a95857df93afdbbdae9e51b7a2e7402c43c2ea88
mizok <cairamgo@gmail.com>
2023-10-24 14:31:53 -0300

    Se agrega base para dropdown de opciones en songcard.

--- src/components/SongCard.vue
+++ src/components/SongCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div @click="playSong">
+    <div @click="playSong" @contextmenu.prevent="doSomething">
         <div class="bg-[#181818] hover:bg-[#282828] p-4 rounded-md m-2 shadow-xlhover:bg-[#252525] cursor-pointer h-[260px] w-[130px] sm:w-[180px] group transition-all duration-300 ease-in-out">
             <div class="flex justify-center items-center relative h-[150px] w-[100px] sm:w-[148px] transition-all duration-300 ease-in-out">
                 <img class="rounded-md shadow-2xl" :src="coverURL" alt="">
@@ -35,5 +35,9 @@ const { coverURL, name, artist } = toRefs(props.song);
 
 const playSong = () => {
     playerStore.playSong(props.song);
+};
+
+const doSomething = () => {
+    console.log('do something');
 };
 </script>
