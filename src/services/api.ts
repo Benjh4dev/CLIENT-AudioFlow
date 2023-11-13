2c05b12432023224603a19cf48ee7c7425636e0c
mizok <cairamgo@gmail.com>
2023-11-13 02:01:32 -0300

    update.

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const port = 3002;
 // baseURL: `http://localhost:${port}/api`,
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_IP,
+  baseURL: import.meta.env.VITE_BACKEND_IP_2,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
