343491504e4a372d17b7a547f20d7417c024e962
mizok <cairamgo@gmail.com>
2023-11-21 10:52:00 -0300

    fix: minor bug fixes in createPlaylistModal

--- src/components/modal/CreatePlaylistModal.vue
+++ src/components/modal/CreatePlaylistModal.vue
@@ -28,24 +28,24 @@
                 
                 <!-- Bloque Izquierdo -->
                 <div class="w-1/2 pr-4 gap-5">
-                  <DialogTitle as="h3" class="text-lg font-medium leading-6 text-white mb-4">¡Crea una Playlist!</DialogTitle>
+                  <DialogTitle as="h3" class="text-lg font-medium leading-6 text-white mb-4">¡Crea una nueva Playlist!</DialogTitle>
                   <form @submit.prevent="submitForm" class="mt-6">
                     <div class="mt-4">
-                      <label for="username" class="block text-sm text-gray-400">Nombre de la Playlist</label>
+                      <label for="playlistName" class="block text-sm text-gray-400">Nombre de la Playlist</label>
                       <input 
                         type="text" 
                         id="playlistName" 
                         name="playlistName" 
                         autocomplete="off"
-                        placeholder="@playlistName"
+                        placeholder="Música favorita"
                         v-model="formData.name"
                         class="w-[90%] h-10 my-2 py-3 px-4 block border-6 bg-gray-950 text-white border-gray-200 rounded-md text-sm focus:border-green-500 focus:ring-green-500 shadow-sm ">
-                
+                      <p v-if="errors" class="text-xs text-red-600 mt-2">{{ errors }}</p>
                     </div>
    
                     <div class="mt-8 flex justify-center pr-8">
-                      <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white hover:text-black hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
-                          Crear
+                      <button type="submit" :disabled="isLoading" :class="{'bg-red-600 hover:bg-red-200': isLoading}" class="inline-flex justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white hover:text-black hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+                          Crear Playlist
                       </button>
                     </div>
                 </form>
@@ -70,24 +70,37 @@ import { TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle } fro
 import { CreatePlaylistForm } from '@/interfaces';
 import { createPlaylist } from '@/backend/playlist';
 
-const isOpen = ref<boolean>(true);
+import { showSuccessToast } from '@/utils/toast';
 
-function closeModal(): void {
-  isOpen.value = false;
-}
+const isOpen = ref<boolean>(true);
+const isLoading = ref<boolean>(false);
+const errors = ref<string>('');
 
 const formData = ref<CreatePlaylistForm>({
     name: ''
 });
 
+const emits = defineEmits(['close']);
+
+function closeModal(): void {
+  isOpen.value = false;
+  setTimeout(() => { emits('close'); }, 300);
+}
+
 async function submitForm(): Promise<void> {
+  errors.value = '';
+  isLoading.value = true;
+
   try {
     await createPlaylist(formData.value);
     closeModal();
+    showSuccessToast('Playlist creada con éxito');
   }
   catch (error: any) {
-    console.log('hubo un error', error)
+    if(error.response.data && error.response.data.issues) {
+      errors.value = error.response.data.issues[0].message;
+    }
+    isLoading.value = false;
   } 
 };
-
 </script>
